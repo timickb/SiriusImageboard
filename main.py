b4f75889c943a3e38e923cb85ce1c4c4926b9c0e
timickb <timikm2002@gmail.com>
2018-11-18 09:27:29 +0300

    Added error handlers

--- main.py
+++ main.py
@@ -43,155 +43,172 @@ def handle_file(file_):
 
 @app.route('/', methods=['GET'])
 def index():
-    topics = database.getTopicsList()
-    result = {
-        'logged': False,
-        'status': -1,
-        'data': topics,
-        'userdata': -1
-    }
-    if 'login' in session:
-        result['logged'] = True
-        result['userdata'] = database.getUserByLogin(session['login'])
+    try:
+        topics = database.getTopicsList()
+        result = {
+            'logged': False,
+            'status': -1,
+            'data': topics,
+            'userdata': -1
+        }
+        if 'login' in session:
+            result['logged'] = True
+            result['userdata'] = database.getUserByLogin(session['login'])
+    except:
+        result['status'] == 'An error occured'
     return render_template('index.html', result=result)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    result = {
-        'logged': False,
-        'status': -1,
-        'data': -1,
-        'userdata': -1
-    }
-    if request.method == 'GET':
-        if 'login' in session:
-            result['logged'] = True
-            result['userdata'] = database.getUserByLogin(session['login'])
-        return render_template('login.html', result=result)
-    elif request.method == 'POST':
-        if 'login' not in session:
-            rresult = database.loginUser(request.form.get('login'), request.form.get('password'))
-            if rresult['status'] == 'OK':
-                session['login'] = request.form.get('login')
-                return redirect('')
-            elif rresult['status'] == 'Error':
-                result['status'] = 'Wrong login or password'
-                return render_template('login.html', result=result)
-        else:
-            result['logged'] = True
-            result['userdata'] = database.getUserByLogin(session['login'])
-            result['status'] = 'You are already logged in'
-            return render_template('login.html', result=result)
+    try:
+        result = {
+            'logged': False,
+            'status': -1,
+            'data': -1,
+            'userdata': -1
+        }
+        if request.method == 'GET':
+            if 'login' in session:
+                result['logged'] = True
+                result['userdata'] = database.getUserByLogin(session['login'])
+        elif request.method == 'POST':
+            if 'login' not in session:
+                rresult = database.loginUser(request.form.get('login'), request.form.get('password'))
+                if rresult['status'] == 'OK':
+                    session['login'] = request.form.get('login')
+                    return redirect('')
+                elif rresult['status'] == 'Error':
+                    result['status'] = 'Wrong login or password'
+            else:
+                result['logged'] = True
+                result['userdata'] = database.getUserByLogin(session['login'])
+                result['status'] = 'You are already logged in'
+    except:
+        result['status'] = 'An error occured'
+    return render_template('login.html', result=result)
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    result = {
-        'logged': False,
-        'status': -1,
-        'data': -1,
-        'userdata': -1
-    }
-    if 'login' not in session:
-        if request.method == 'GET':
-            return render_template('register.html', result=result)
-        elif request.method == 'POST':
-            # register user
-            login = request.form.get('login')
-            password = request.form.get('password')
-            rpassword = request.form.get('rpassword')
-            email = request.form.get('email')
-
-            rresult = database.regUser(login, password, rpassword, email)
-            if rresult['status'] == 'OK':
-                session['login'] = login
-                return redirect('')
-            elif rresult['status'] == 'Error':
-                result['status'] = rresult['text']
+    try:
+        result = {
+            'logged': False,
+            'status': -1,
+            'data': -1,
+            'userdata': -1
+        }
+        if 'login' not in session:
+            if request.method == 'GET':
                 return render_template('register.html', result=result)
-    else:
-        return redirect('')
+            elif request.method == 'POST':
+                # register user
+                login = request.form.get('login')
+                password = request.form.get('password')
+                rpassword = request.form.get('rpassword')
+                email = request.form.get('email')
+
+                rresult = database.regUser(login, password, rpassword, email)
+                if rresult['status'] == 'OK':
+                    session['login'] = login
+                    return redirect('')
+                elif rresult['status'] == 'Error':
+                    result['status'] = rresult['text']
+                    return render_template('register.html', result=result)
+        else:
+            return redirect('')
+    except:
+        result['status'] = 'An error occured'
+        return render_template('register.html', result=result)
         
 @app.route('/settings', methods=['GET', 'POST'])
 def settings():
-    result = {
-        'logged': False,
-        'status': -1,
-        'data': -1,
-        'userdata': -1
-    }
-    if 'login' in session:
-        result['logged'] = True
-        result['userdata'] = database.getUserByLogin(session['login'])
-        if request.method == 'GET':
-            return render_template('settings.html', result=result)
-        elif request.method == 'POST':
-            uid = database.getUserIDByLogin(session['login'])
-            if request.form.get('change-email') != None:
-                print('AA  IISSJI JJKDSJKDSKJDSJKDSKJDSJK!!!')
-                oldEmail = request.form.get('change-email-old')
-                newEmail = request.form.get('change-email-new')
-                r = database.changeEmail(uid, oldEmail, newEmail)
-                if r == 'OK':
-                    result['status'] == 'Success'
-                else:
-                    result['status'] == 'Incorrect email'
+    try:
+        result = {
+            'logged': False,
+            'status': -1,
+            'data': -1,
+            'userdata': -1
+        }
+        if 'login' in session:
+            result['logged'] = True
+            result['userdata'] = database.getUserByLogin(session['login'])
+            if request.method == 'GET':
                 return render_template('settings.html', result=result)
-            elif request.form.get('change-password') != None:
-                oldPassword = request.form.get('change-password-old')
-                newPassword = request.form.get('change-password-new')
-                rnewPassword = request.form.get('change-password-rnew')
-                r = database.changePassword(uid, oldPassword, newPassword, rnewPassword)
-                if r == 'OK':
-                    result['status'] == 'Success'
+            elif request.method == 'POST':
+                uid = database.getUserIDByLogin(session['login'])
+                if request.form.get('change-email') != None:
+                    print('AA  IISSJI JJKDSJKDSKJDSJKDSKJDSJK!!!')
+                    oldEmail = request.form.get('change-email-old')
+                    newEmail = request.form.get('change-email-new')
+                    r = database.changeEmail(uid, oldEmail, newEmail)
+                    if r == 'OK':
+                        result['status'] == 'Success'
+                    else:
+                        result['status'] == 'Incorrect email'
+                    return render_template('settings.html', result=result)
+                elif request.form.get('change-password') != None:
+                    oldPassword = request.form.get('change-password-old')
+                    newPassword = request.form.get('change-password-new')
+                    rnewPassword = request.form.get('change-password-rnew')
+                    r = database.changePassword(uid, oldPassword, newPassword, rnewPassword)
+                    if r == 'OK':
+                        result['status'] == 'Success'
+                    else:
+                        result['status'] == 'Incorrect password or passwords do not match'
                 else:
-                    result['status'] == 'Incorrect password or passwords do not match'
-            else:
-                return render_template('settings.html', result=result)
-    else:
-        return redirect('login')
+                    return render_template('settings.html', result=result)
+        else:
+            return redirect('login')
+    except:
+        result['status'] = 'An error occured'
+        return render_template('settings.html', result=result)
 
 @app.route('/topic/<topicID>/', methods=['GET', 'POST'])
 def topic(topicID):
-    result = {
-        'logged': False,
-        'status': -1,
-        'data': -1,
-        'userdata': -1
-    }
-    messages = database.getMessages(topicID)
-    info = database.getTopicByID(topicID)
-    result['data'] = {'info': info, 'messages': messages}
-    if request.method == 'GET':
-        if 'login' in session:
-            result['logged'] = True
-            result['userdata'] = database.getUserByLogin(session['login'])
-            database.checkTopicStatus(topicID)
+    try:
+        result = {
+            'logged': False,
+            'status': -1,
+            'data': -1,
+            'userdata': -1
+        }
+        messages = database.getMessages(topicID)
+        info = database.getTopicByID(topicID)
+        result['data'] = {'info': info, 'messages': messages}
+        if request.method == 'GET':
+            if 'login' in session:
+                result['logged'] = True
+                result['userdata'] = database.getUserByLogin(session['login'])
+                database.checkTopicStatus(topicID)
+            return render_template('topic.html', result=result)
+        elif request.method == 'POST':
+            if 'login' in session:
+                result['logged'] = True
+                result['userdata'] = database.getUserByLogin(session['login'])
+
+                text = request.form.get('text')
+
+                file_ = None
+                try:
+                    file_ = request.files['file']
+                except:
+                    count = 0
+                if file_: count = 1
+
+                r = database.postMessage(text, topicID, database.getUserIDByLogin(session['login']), count)
+                if r == 'Error':
+                    result['status'] = 'Invalid data'
+                    return render_template('topic.html', result=result)
+                elif r == 'OK':
+                    if file_ and handle_file(file_):
+                        filename = str(database.getNextMessageID()-1)+'.jpg'
+                        file_.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                return redirect('topic/'+topicID+'/')
+            else:
+                return redirect('login')
+    except:
+        result['status'] = 'An error occured'
         return render_template('topic.html', result=result)
-    elif request.method == 'POST':
-        if 'login' in session:
-            result['logged'] = True
-            result['userdata'] = database.getUserByLogin(session['login'])
-
-            text = request.form.get('text')
-
-            file_ = None
-            try:
-                file_ = request.files['file']
-            except:
-                count = 0
-            if file_: count = 1
-
-            r = database.postMessage(text, topicID, database.getUserIDByLogin(session['login']), count)
-            if r == 'Error':
-                result['status'] = 'Invalid data'
-                return render_template('topic.html', result=result)
-            elif r == 'OK':
-                if file_ and handle_file(file_):
-                    filename = str(database.getNextMessageID()-1)+'.jpg'
-                    file_.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect('topic/'+topicID+'/')
-        else:
-            return redirect('login')
 
 @app.route('/logout', methods=['GET'])
 def logout():
